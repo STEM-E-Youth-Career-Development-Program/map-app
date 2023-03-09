5f0865dbd57df4e0273621b4b299f28d1b81f152
Oluwasemilore Adegoroye <oluwasemilore508@gmail.com>
2023-03-08 18:05:33 -0600

    Built the screen component

---
+++ app/components/Screen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import Constants from 'expo-constants';
+
+function Screen({ children, style }) {
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={[styles.view, style]}>{children}</View>
+    </SafeAreaView>
+  );
+}
+
+export default Screen;
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+  view: {
+    flex: 1,
+  },
+});
