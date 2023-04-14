dc051a32f5cc0fe1e9a9d564aa1251b41d8cb795
Oluwasemilore Adegoroye <oluwasemilore508@gmail.com>
2023-04-14 11:57:08 -0500

    Built the Submit Button Component

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Event from './app/components/Event';
 import CreateEventScreen from './app/screens/CreateEventScreen';
 import Screen from './app/components/Screen';
+import SubmitButton from './app/components/SubmitButton';
 
 export default function App() {
   return (
     <Screen>
-      <Event />
+      {/* <Event /> */}
+      <SubmitButton />
     </Screen>
   );
 }
---
+++ app/components/SubmitButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, TouchableWithoutFeedback, Text } from 'react-native';
+import { useFormikContext } from 'formik';
+
+function SubmitButton({ title }) {
+  const { handleSubmit } = useFormikContext();
+  return (
+    <TouchableWithoutFeedback onPress={handleSubmit}>
+      <Text style={styles.button}>{title}</Text>
+    </TouchableWithoutFeedback>
+  );
+}
+
+export default SubmitButton;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#000',
+    borderRadius: 10,
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
