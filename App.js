4351f4654a01af5b8686e9f5f0bbe1eb2e17df89
Oluwasemilore Adegoroye <oluwasemilore508@gmail.com>
2023-04-18 18:36:24 -0500

    Built the search bar component

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import CreateEventScreen from './app/screens/CreateEventScreen';
 import Screen from './app/components/Screen';
 import SubmitButton from './app/components/SubmitButton';
 import EventListScreen from './app/screens/EventListScreen';
+import SearchBar from './app/components/SearchBar';
 
 export default function App() {
-  return <EventListScreen />;
+  return (
+    <Screen>
+      <SearchBar />
+    </Screen>
+  );
 }
---
+++ app/components/SearchBar.js
@@ -0,0 +1,49 @@
+import React, { useRef } from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+function SearchBar(props) {
+  const searchRef = useRef(null);
+
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={() => searchRef.current.focus()}>
+        <MaterialCommunityIcons name="magnify" size={33} />
+      </TouchableWithoutFeedback>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          style={styles.textInput}
+          ref={searchRef}
+          placeholder="Search for event"
+        />
+      </View>
+    </View>
+  );
+}
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    borderColor: '#999',
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: 'row',
+    marginVertical: 10,
+    padding: 10,
+  },
+  textInput: {
+    fontSize: 18,
+    marginLeft: 10,
+  },
+  textInputContainer: {
+    width: '100%',
+  },
+});
