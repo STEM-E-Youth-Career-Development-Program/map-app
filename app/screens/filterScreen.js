3e2d0643eac93fdfb8e8d8a841c2f2f7156e3e8f
zoe-kch <138423129+zoe-kch@users.noreply.github.com>
2023-07-09 17:34:14 -0600

    Create filterScreen.js

---
+++ app/screens/filterScreen.js
@@ -0,0 +1,199 @@
+import { useState } from 'react';
+import { View, SafeAreaView, Text, StyleSheet, Pressable, StatusBar } from 'react-native';
+import { MultipleSelectList, SelectList } from 'react-native-dropdown-select-list'
+import Slider from '@react-native-community/slider';
+import Checkbox from 'expo-checkbox';
+import { LinearGradient } from 'expo-linear-gradient';
+
+
+const filterScreen = () => {
+  const [selected, setArea] = useState('');
+  const [distance, setDistance] = useState(40);
+  var distanceText = distance + ' km';
+  if (distance == 160) {
+    distanceText = '160+ km'
+  } else if (cost == 0) {
+    distanceText = 'Free'
+  };
+  const [cost, setCost] = useState(0);
+  var costText = 'Max. $' + cost;
+  if (cost == 1000) {
+    costText = '$1000+'
+  } else if (cost == 0) {
+    costText = 'Free'
+  };
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+  const areas = [
+    {value:'Science'},
+    {value:'Technology'},
+    {value:'Engineering'},
+    {value:'Math'},
+    {value:'Entrepreneurship'},
+  ]
+  const [Yes, setYes] = useState(false);
+  const [No, setNo] = useState(false);
+  var yestextColor = 'grey';
+  var notextColor = 'grey';
+  if (Yes == (true)) {
+    yestextColor = 'black'
+  };
+  if (No == (true)) {
+    notextColor = 'black'
+  };
+
+  return (
+    <SafeAreaView style={{backgroundColor: '#EEEEEE', flex: 1}}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Filter & Sort</Text>
+      </View>
+
+      <Text style={styles.heading}>Subject</Text>
+      <View style={styles.line} />
+      <View style={{paddingHorizontal: 16, paddingTop: 15}}>
+        <MultipleSelectList
+          setSelected={(area) => setArea(area)}
+          data={areas} 
+          save="value"
+          placeholder="Select STEME Area"
+          searchPlaceholder='Select STEME Area'
+          search={false}
+        />
+      </View>
+      
+      <Text style={[styles.heading, {marginTop: '5%'}]}>Distance</Text>
+      <View style={styles.line} />
+      <Slider
+        style={styles.slider}
+        minimumValue={40}
+        maximumValue={160}
+        step={10}
+        onValueChange={setDistance}
+        minimumTrackTintColor='#999999'
+        thumbTintColor='grey'
+      />
+      <View style={styles.sliderLabels}>
+        <Text style={styles.sliderText}>{'≤'}40 km</Text>
+        <Text style={{color: '#5A5A5A'}}>Within {distanceText}</Text>
+        <Text style={styles.sliderText}>160+ km</Text>
+      </View>
+
+      <Text style={styles.heading}>Cost</Text>
+      <View style={styles.line} />
+      <Slider
+        style={styles.slider}
+        minimumValue={0}
+        maximumValue={1000}
+        step={10}
+        onValueChange={setCost}
+        minimumTrackTintColor='#999999'
+        thumbTintColor='grey'
+      />
+      <View style={styles.sliderLabels}>
+        <Text style={styles.sliderText}>$0</Text>
+        <Text style={{color: '#5A5A5A'}}>{costText}</Text>
+        <Text style={styles.sliderText}>$1000+</Text>
+      </View>
+
+      <Text style={styles.heading}>Meal Included</Text>
+      <View style={styles.line} />
+      <View style={{flexDirection: 'row'}}>
+        <Checkbox
+          style={styles.checkbox}
+          value={Yes}
+          onValueChange={setYes}
+          color='grey'
+        />
+        <Text style={[styles.checkbox, {color: yestextColor}]}>Yes</Text>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <Checkbox
+          style={styles.checkbox}
+          value={No}
+          onValueChange={setNo}
+          color='grey'
+        />
+        <Text style={[styles.checkbox, {color: notextColor}]}>No</Text>
+      </View>
+
+      <LinearGradient
+        colors={['black', '#5A5A5A']}
+        style={styles.button}
+        locations={[0.1, 0.9]}
+      >
+        <Pressable
+          onPress={() => {
+            //change screen
+          }}
+        >
+          <Text style={styles.apply}>Apply</Text>
+        </Pressable>
+      </LinearGradient>
+    </SafeAreaView>
+  )
+}
+
+export default filterScreen;
+
+const styles = StyleSheet.create({
+  header: {
+    height: 50,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+    flexDirection: 'row',
+  },
+  heading: {
+    fontWeight: 800,
+    fontSize: 15,
+    marginTop: '10%',
+    marginLeft: '4%',
+  },
+  title: {
+    fontWeight: 800,
+    fontSize: 15,
+    alignSelf: 'center',
+  },
+  line: {
+    backgroundColor: '#999999',
+    alignSelf: 'center',
+    height: '.2%',
+    width: '92%',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    width: '92%',
+    height: '6%',
+    alignSelf: 'center',
+    bottom: 15,
+    borderRadius: 8,
+  },
+  apply: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  slider: {
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+  sliderLabels: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: 5,
+  },
+  sliderText: {
+    fontSize: 15,
+    color: '#999999',
+    fontWeight: 300,
+  },
+  checkbox: {
+    marginLeft: '4%',
+    marginTop: 15,
+    fontWeight: 800,
+  }
+})
