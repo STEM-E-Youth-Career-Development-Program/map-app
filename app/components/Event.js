fb08d3a8f8e28bce9c5df3d55c6a78da7da06e4a
zoe-kch <138423129+zoe-kch@users.noreply.github.com>
2023-07-17 17:32:02 -0600

    Updated Event.js

--- app/components/Event.js
+++ app/components/Event.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { View, StyleSheet, Image, Text } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-function Event({ heading, date, subject }) {
+function Event({ heading, startDate, endDate, subject, distance, cost, meal }) {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={{shadowColor: 'black', shadowOffset: {width: 0, height: 5}, shadowOpacity: .34, shadowRadius: 6.27, elevation: 10}}>
         <Image
-          style={{ width: 85, height: 85 }}
+          style={{ width: 85, height: 85, borderRadius: 85 }}
           source={require('../assets/eventIcon.png')}
         />
       </View>
       <View>
         <Text style={styles.heading}>{heading}</Text>
-        <Text style={styles.date}>{date}</Text>
-        <Text style={styles.date}>{subject}</Text>
+        <Text style={{fontSize: 10}}>{startDate} - {endDate}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{fontSize: 10}}>Subject: {subject}{'   '}</Text>
+          <Text style={{fontSize: 10}}>{distance} mi{'    '}</Text>
+          <Text style={{fontSize: 10}}>${cost}{'    '}</Text>
+          <Text style={{fontSize: 10}}>{meal}</Text>
+        </View>
       </View>
-      <View>
+      <View style={{marginHorizontal: 100}}>
         <MaterialCommunityIcons name="map-marker-radius" size={45} />
       </View>
     </View>
@@ -29,11 +34,12 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-  },
-  date: {
-    fontSize: 10,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    width: '95%',
+    alignSelf: 'center'
   },
   heading: {
     fontWeight: 'bold',
