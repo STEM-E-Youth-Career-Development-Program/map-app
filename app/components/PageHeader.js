ff2105f7b9520c80c82470a234c75e20350f7119
zoe-kch <138423129+zoe-kch@users.noreply.github.com>
2023-07-17 15:09:32 -0600

    added back arrow

--- app/components/PageHeader.js
+++ app/components/PageHeader.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { MaterialCommunityIcons} from '@expo/vector-icons'
 
 function PageHeader({ header }) {
   return (
     <View style={styles.container}>
+      <MaterialCommunityIcons name="arrow-left" size={40} style={styles.backButton}/>
       <Text style={styles.header}>{header}</Text>
     </View>
   );
@@ -12,11 +14,23 @@ function PageHeader({ header }) {
 export default PageHeader;
 
 const styles = StyleSheet.create({
-  header: {
-    color: '#000',
-    fontWeight: 'bold',
-    fontSize: 35,
-    textAlign: 'center',
-    marginVertical: 20,
+  container: {
+    height: 50,
+    width: '100%',
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'stretch',
   },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    position: 'absolute',
+    alignSelf: 'center',
+    flex: 1,
+  },
+  backButton: {
+    flex: 1,
+    alignSelf: 'center',
+  }
 });
