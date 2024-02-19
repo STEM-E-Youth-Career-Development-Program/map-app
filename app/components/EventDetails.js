316cae730d849d35c48438773d5e2531bd8f1daf
MenlyCSE <MenlyCSE@gmail.com>
2024-02-18 20:25:01 -0500

    formatDate
    This variable is already declared with the same properties at a previous line.

--- app/components/EventDetails.js
+++ app/components/EventDetails.js
@@ -45,11 +45,6 @@ const EventDetails = (props) => {
     }
   };
 
-
-  const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-  };
   return (
     <>
       <View style={{ paddingTop: Constants.statusBarHeight }}>
